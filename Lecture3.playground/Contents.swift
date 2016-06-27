b1dbdbaf4c387cf8898d86a63dab78c4664a9022
ejonassen <jonassen.eric@gmail.com>
2016-06-27 16:28:36 -0400

    Playing Around with Swift

--- Lecture3.playground/Contents.swift
+++ Lecture3.playground/Contents.swift
@@ -7,4 +7,23 @@ var str = "Hello, playground"
 
 
 str = "another string"
-immutableString = "somes string"
+
+var myInt = 42
+let myConstant = 50/100
+
+
+let implicitDouble = 70.0
+let explicitDouble: Double = 70
+
+let label = "the width is "
+let width = 94
+let widthLabel = label + String(width)
+String(94)
+let apples = 3
+let oranges = 5
+let appleSummary = "I have \(apples) apples."
+let orangeSummary = "I have \(oranges) oranges."
+
+
+var shoppingList = ["catfish", "water", "tulips", "blue paint"]
+shoppingList.append("toothpaste")
